72bf491fcad2639db63ebc8c4823d5ebfd8dd0b1
Bubamara0 <72171393+Bubamara0@users.noreply.github.com>
2021-10-11 21:23:40 +0200

    Create exercice3.py

---
+++ python/exercice3.py
@@ -0,0 +1,15 @@
+# Saisie des trois nombres
+a = int(input("a : "))
+b = int(input("b : "))
+c = int(input("c : "))
+
+# Comparaison et affichage du plus grand nombre
+if a > b & a > c :
+    print("a est le plus grand")
+elif b > a & b > c :
+    print("b est le plus grand")
+elif c > a & c > b :
+    print("c est le plus grand")
+    
+
+  
